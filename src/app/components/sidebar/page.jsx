bc7d11fce50ae2080e8748fac54023e4d48c769d
Raahat Khan <raahatctrack001.rc@gmail.com>
2025-03-25 23:23:00 +0530

    design/modify sidebar

--- src/app/components/sidebar/page.jsx
+++ src/app/components/sidebar/page.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+'use client';
 
-const Sidebar = () => {
+import Link from 'next/link';
+import { FaXTwitter } from 'react-icons/fa6';
+import { HiHome, HiDotsHorizontal } from 'react-icons/hi';
+
+export default function Sidebar() {
+  const session = null;
   return (
-    <div> Sidebar </div>
-  )
+    <div className='flex flex-col p-3 justify-between h-screen'>
+      <div className='flex flex-col gap-4 '>
+        <Link href='/'>
+          <FaXTwitter className='w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200 ' />
+        </Link>
+        <Link
+          href='/'
+          className='flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit'
+        >
+          <HiHome className='w-7 h-7' />
+          <span className='font-bold hidden xl:inline'>Home</span>
+        </Link>
+        
+          <button
+            className='bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'
+          >
+            {session ? "Sign Out" : "Signin"}
+          </button>
+         
+      </div>
+      
+    </div>
+  );
 }
-
-export default Sidebar
